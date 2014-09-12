6839d49d16062e426851fea0c7839749af1199a0
rkorsak <rkorsak@gmail.com>
2014-09-11 22:12:40 -0400

    Added just a bit of JSON response generating and a dummy html index. Still no trace of either bingo or bango

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject bingobango "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A bingo game for your home"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler bingobango.handler/app}
   :profiles
--- src/bingobango/handler.clj
+++ src/bingobango/handler.clj
@@ -1,12 +1,17 @@
 (ns bingobango.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :refer [resource-response response]]
+            [ring.middleware.json :as middleware]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
+  (GET "/widgets" [] (response [{:id "1" :name "TOMMY"} {:id "2" :name "RAYAN"}]))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (-> (handler/site app-routes)
+  	  (middleware/wrap-json-body)
+  	  (middleware/wrap-json-response)))
